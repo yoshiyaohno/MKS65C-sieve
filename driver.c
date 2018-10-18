481d921a130b89f24505431affa1854573c45cc3
Yoshiya <yohno@stuy.edu>
2018-10-18 19:04:52 -0400

    Add driver file

---
+++ driver.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sieve.h"
+
+int main(int argc, char * argv[]) {
+    int iterations = 1;
+    int target = 1000000;
+    if(argc > 1) {
+        target = atoi(argv[1]);
+    }
+    if(argc > 2) {
+        iterations = atoi(argv[2]);
+    }
+
+
+    int ans = 0;
+    while(iterations>0) {
+        ans=sieve(target);
+        iterations--;
+        printf("The n=%d prime is %d\n", target, ans );
+        //this is to modify which prime to
+        //potentially avoid CPU caching
+        target++;
+    }
+    return 0;
+}
---
+++ sieve.h
@@ -0,0 +1,1 @@
+int sieve(int);
