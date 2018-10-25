306ca1f42063739d587f348c99322b564c8d919a
Yoshiya <yohno@stuy.edu>
2018-10-24 22:43:03 -0400

    aaaaaaaaaaaaaaaaaaaa

--- sieve.c
+++ sieve.c
@@ -4,6 +4,8 @@
 #include "sieve.h"
 
 //////// BITWISE ////////
+// peff and tracer not pointers,
+// looking at maybe 0.089
 
 //// SOME OPTIMIZATIONS ////
 //
@@ -19,32 +21,48 @@ int sieve( int n )
 {
     int size;
     if( n < 5000 )
-        size = (1.3 * n * log(n) + 10) / 16;
+        size = (1.3 * n * log(n) + 10) / 2;
     else
-        size = 1.15 * n * log(n) / 16;
+        size = 1.15 * n * log(n) / 2;
 
     // printf("\t\tSIZE %d\n", size);
-    char *jeff = calloc( size, 1);
+    char *jeff = calloc( size/8, 1);
 
     int peff;
     int tracer = 0;
 
+    int wheel[]   = {1, 7, 11, 13, 17, 19, 23, 29};
+    //int wheel[] = {1, 7, 11, 13, 17, 19, 23, 29};
+    int acc[]     = {6, 4, 2, 4, 2, 4, 6, 2};
+    char hrm = 0;
+    int w_trac = 0;
+
     // printf("PRE-SIEV\tn is %d\n", n);
 
-    int size_sqrt = sqrt(size * 8);
+
+    int size_sqrt = sqrt(size);
     while( tracer < size_sqrt ) {
-        while( jeff[(++tracer)/8] & 1<<(tracer%8) );
+        // while( jeff[(tracer = 30*(++w_trac/8) + wheel[w_trac%8])/8]
+        //         & 1<<(tracer%8) );
+        while( jeff[ (tracer += acc[hrm]) / 8 ] & 1<<(tracer%8)) {
+            if(hrm >= 8) hrm = 0;
+            printf("looking hrm %d\n", hrm);
+        }
         n--;
-        // printf("TRACER: %d\n", tracer);
         peff = tracer;
-        while( (peff += 2*tracer+1) < size * 8 )
+
+        // DO 2 * n + 1   DO NOT FORGET OK OK OK 
+        while( (peff += tracer) < size ) {
             jeff[ peff/8 ] |= 1<<(peff%8);
-        // printf("IN-SIEV\tn is %d\n", n);
+        }
     }
 
     while(--n)
-        while( jeff[ (++tracer)/8] & 1<<(tracer%8) );
-    return 2*tracer+1;
+        // while( jeff[(tracer = 30*(++w_trac/8) + wheel[w_trac%8])/8]
+        //         & 1<<(tracer%8) );
+        while( jeff[ (tracer += acc[hrm++]) /8 ] & 1<<(tracer%8))
+            if( hrm >= 8 ) hrm = 0;
+    return tracer;
 }
 
 // int main()
--- wheel_sieve.c
+++ wheel_sieve.c
@@ -3,54 +3,53 @@
 #include <math.h>
 #include "sieve.h"
 
-//////// THIS ONE IS NOT BITWISE ////////
+//////// BITWISE ////////
+// peff and tracer not pointers,
+// looking at maybe 0.089
 
 //// SOME OPTIMIZATIONS ////
 //
 //    still to do
-// 3. minimise writes, short circuit, etc
-// 4. b i t w i s e    o p e r a t i o n s
 // 5. see if you can find a way around calloc
 //      (multiple varaibles? lol)
+// 5.5? I guess make tracer a pointer, see if that helps
+// 6. W H E E L    T I M E 
 //
 //// SOME OPTIMIZATIONS ////
 
 int sieve( int n )
 {
-
     int size;
     if( n < 5000 )
         size = (1.3 * n * log(n) + 10) / 2;
     else
         size = 1.15 * n * log(n) / 2;
 
-    printf("\t\tSIZE %d\n", size);
-    char *jeff = calloc( size, 1);
+    // printf("\t\tSIZE %d\n", size);
+    char *jeff = calloc( size/8, 1);
 
-    char *p;
+    int peff;
     int tracer = 0;
 
     int wheel[] = {1, 7, 11, 13, 17, 19, 23, 29};
-    int w_turns = 0;
-    char w_offset = 0;
+
     // printf("PRE-SIEV\tn is %d\n", n);
+    int real_gdp;
 
     int size_sqrt = sqrt(size);
     while( tracer < size_sqrt ) {
-        while( jeff[++tracer] );
-        w_turns = tracer / 30;
-        w_offset = tracer & 0b111;
+        while( jeff[(++tracer)/8] & 1<<(tracer%8) );
         n--;
-        printf("TRACER: %d\n", tracer);
-        p = jeff + tracer;
-        while( (p += 30*w_turns + wheel[w_offset]) < jeff + size )
-            *p = 1;
-        // printf("IN-SIEV\tn is %d\n", n);
+        peff = tracer;
+
+        real_gdp = 30*(tracer/8)+wheel[tracer%8];
+        while( (peff += real_gdp) < size )
+            jeff[ peff/8 ] |= 1<<(peff%8);
     }
 
     while(--n)
-        while( jeff[ ++tracer] );
-    return 30*w_turns + wheel[w_offset];
+        while( jeff[ (++tracer)/8] & 1<<(tracer%8) );
+    return 30*(tracer/8)+wheel[tracer%8];
 }
 
 // int main()
